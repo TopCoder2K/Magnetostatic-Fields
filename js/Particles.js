75b1993204bf236dc48b4d11aa1ee64134cafa55
TopCoder2000 <pchelintsev.sv@phystech.edu>
2020-01-01 23:38:48 +0300

    Axes were added.

---
+++ js/Particles.js
@@ -0,0 +1,21 @@
+/*let Config = {
+    'colors': {
+        'white': (255, 255, 255),
+        'black': (0, 0, 0),
+        'green': (0, 100, 0),
+        'red': (100, 0, 0)
+    }
+};*/
+
+let Config = {
+    'window': {
+        'height': 600,
+        'width': 600,
+    },
+    'colors': {
+        'white': [255, 255, 255],
+        //'black': (0, 0, 0),
+        //'green': (0, 100, 0),
+        //'red': (100, 0, 0)
+    }
+};
