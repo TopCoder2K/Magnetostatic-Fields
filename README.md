2d186263789fad4bd94691dd3aa01a0d0c6627c1
TopCoder2000 <pchelintsev.sv@phystech.edu>
2020-01-05 00:25:54 +0300

    Small improvements

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 To start the visualisation run **index.html**.
 
 Use the menu at the top left-hand corner to vary amperage and permeability.
-There are different circuits which can be chosen by changing the circuit_name in the **Config.js**.
-Also do not forget about "balanced values" for every circuit for a better visualisation. 
+There are different circuits that can be chosen by changing the circuit_name in the **Config.js**.
+Also do not forget about "balanced values" for every circuit for better visualisation. 
 
 Enjoy!
--- js/VectorExtension.js
+++ js/VectorExtension.js
@@ -1,12 +1,12 @@
 // As library functions are bad, rewrite them.
 // -u
-function Negate(u) {
+function negate(u) {
     let copied_u = new THREE.Vector3(0, 0, 0);
     copied_u.copy(u);
     return copied_u.negate();
 }
 // v + u
-function Add(v, u) {
+function add(v, u) {
     let result = new THREE.Vector3(0, 0, 0);
     result.add(v);
     result.add(u);
@@ -14,12 +14,12 @@ function Add(v, u) {
     return result;
 }
 // v - u
-function Subtract(v, u) {
+function subtract(v, u) {
     let copied_u = new THREE.Vector3(0, 0, 0);
     copied_u.copy(u);
     const negated_u = copied_u.negate();
 
-    return Add(v, negated_u);
+    return add(v, negated_u);
 }
 // v x u
 function crossProduct(v, u) {
@@ -33,7 +33,7 @@ function addScaledVector(v, u, k) {
     scaled_u.copy(u);
     scaled_u.multiplyScalar(k);
 
-    return Add(v, scaled_u);
+    return add(v, scaled_u);
 }
 // v - u * k
 function subtractScaledVector(v, u, k) {
@@ -41,7 +41,7 @@ function subtractScaledVector(v, u, k) {
     scaled_u.copy(u);
     scaled_u.multiplyScalar(k);
 
-    return Subtract(v, scaled_u);
+    return subtract(v, scaled_u);
 }
 // v * u
 function dotProduct(v, u) {
